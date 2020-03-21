a7c3fc5de4eac30a758dfaa48ac6ec662c82d564
hashlash <muh.ashlah@gmail.com>
2020-03-22 00:14:58 +0700

    add server1 upload file form

---
+++ server1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./pages/home.html")
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		home(w, r)
+	} else if r.Method == http.MethodPost {
+		upload(w, r)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", handler)
+	http.Handle("/static/", http.FileServer(http.Dir("./static")))
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
+
+func main() {
+	setupRoutes()
+}
