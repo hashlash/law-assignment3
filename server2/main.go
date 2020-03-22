4de09ae4268c667e3801d4419758017bb2620965
hashlash <muh.ashlah@gmail.com>
2020-03-23 00:03:46 +0700

    add compression and it's progress

--- server2/main.go
+++ server2/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
 	"github.com/streadway/amqp"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"time"
 )
 
-func rmqSend(routingKey string) {
+func rmqSend(file multipart.File, routingKey string) {
 	time.Sleep(3 * time.Second)
 	conn, err := amqp.Dial(os.Getenv("AMQP_URL"))
 	if err != nil {
@@ -39,10 +43,40 @@ func rmqSend(routingKey string) {
 		return
 	}
 
-	counter := 10
-	for counter <= 100 {
-		body := fmt.Sprintf("%d%%", counter)
-		fmt.Printf("sending %s\n", body)
+	fBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Println("Failed to read file:", err)
+		return
+	}
+
+	var buff bytes.Buffer
+	gzw, err := gzip.NewWriterLevel(&buff, gzip.BestCompression)
+	if err != nil {
+		log.Println("Failed to create gzip writer", err)
+		return
+	}
+	defer gzw.Close()
+
+	part := 1
+	partLen := (len(fBytes) + 9) / 10
+
+	for part <= 10 {
+		left := (part-1) * partLen
+		right := part * partLen
+
+		if part == 10 {
+			right = len(fBytes)
+		}
+
+		log.Printf("Compressing %d%%", part*10)
+
+		_, err := gzw.Write(fBytes[left:right])
+		if err != nil {
+			log.Println("Failed to write part ", part, err)
+			return
+		}
+
+		body := fmt.Sprintf("%d%%", part*10)
 
 		msg := amqp.Publishing{
 			ContentType: "text/plain",
@@ -60,14 +94,22 @@ func rmqSend(routingKey string) {
 			log.Println("Failed to publish:", err)
 			continue
 		}
-		counter += 10
+
+		part += 1
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	routingKey := r.Header.Get("X-ROUTING-KEY")
 
-	go rmqSend(routingKey)
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		log.Println("Failed to get the file:", err)
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
+
+	go rmqSend(file, routingKey)
 }
 
 func setupRoutes() {
